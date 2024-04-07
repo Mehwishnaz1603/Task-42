1ae4e2d1d7ba3ddef71f552afa927db6aefe8c9c
Mehwishnaz <160154061+Mehwishnaz1603@users.noreply.github.com>
2024-04-07 17:20:32 +0500

    ,,.

---
+++ index.ts
@@ -0,0 +1,14 @@
+         /////Task # 42 ////
+//Great Magicians: Start with a copy of your program from Exercise 39. 
+//Write a function called make_great() that modifies the array of magicians
+//by adding the phrase the Great to each magician’s name. Call show_magicians()
+//to see that the list has actually been modified.
+
+let magician_name: string[] = ["Harry Houdini", "Peter nelson","Azur russel"]
+function make_great() {
+    magician_name.splice(3, 0, "The Great")
+    console.log(magician_name)
+}
+make_great()
+
+
